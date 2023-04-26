e5fc12fe6a0697307b0e3400260ec9c11cafb771
rdautov <dautov.rustem@gmail.com>
2023-04-26 19:14:41 +0200

    Code clean-up

--- src/DesiredPropertyFeaturesHandle.js
+++
@@ -1,8 +0,0 @@
-import { DefaultFeaturesHandle } from "@eclipse-ditto/ditto-javascript-client-dom"; 
-
-export class DesiredPropertyFeaturesHandle extends DefaultFeaturesHandle {
-  //Constructor
-  constructor() {
-    super();
-  }
-}
